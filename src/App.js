5121e1afcf892d4450a8a9cf3c66ca6c68bfb956
GrantWasil <grantwasil1@gmail.com>
2019-09-15 23:34:17 -0600

    Created a working framework for the project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import axios from "axios";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Components
+import Search from "./Components/Search";
+import Nav from "./Components/Nav";
+import Photo from "./Components/Photo";
+import NotFound from "./Components/NotFound";
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+console.log(API_KEY);
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      photos: [],
+      loading: true
+    };
+  }
+
+  componentDidMount() {
+    this.performSearch();
+  }
+
+  performSearch = (query = "cats") => {
+    axios
+      .get(
+        `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${API_KEY}&tags=${query}&per_page=24&format=json&nojsoncallback=1`
+      )
+      .then(response => {
+        this.setState({photos: response.data.photos.photo, loading: false});
+      })
+      .catch(err => {
+        console.error("Error fetching and parsind data", err);
+      });
+  };
+
+  render() {
+    console.log(this.state.photos);
+    return (
+      <BrowserRouter>
+        <div className="container">
+          <Search onSearch={this.performSearch} />
+          <Nav />
+          <Switch>
+            <Route exact path="/" component={Photo} />
+            <Route component={NotFound} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
-
-export default App;
---
+++ src/Components/PhotoList.js
@@ -0,0 +1,3 @@
+import React from "react";
+import Photo from "./Photo";
+import NotFOund from "./NotFound";
